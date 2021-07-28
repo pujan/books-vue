0043d038836bb45c7b00e0db8aaa62ec005a824c
pujan <lukasz.pelc.81@gmail.com>
2021-07-28 11:59:10 +0200

    Refactoring App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,58 @@
 <template>
-    <nav>
-      <ul class="menu">
-        <li :class="{active: $route.name == 'Home'}"><router-link to="/">Home</router-link></li>
-        <li :class="{active: $route.name == 'Books'}"><router-link to="/books">Books</router-link></li>
-        <li :class="{active: $route.name == 'Authors'}"><router-link to="/authors">Authors</router-link></li>
-      </ul>
-    </nav>
-  <router-view/>
+    <header>
+        <h1 class="header"></h1>
+        <nav>
+            <div class="main-menu">
+                <div class="menu-item" :class="{active: $route.name == 'Home'}">
+                    <router-link to="/"><span>Home</span></router-link>
+                </div>
+                <div class="menu-item" :class="{active: $route.name == 'Books'}">
+                    <router-link to="/books"><span>Books</span></router-link>
+                </div>
+                <div class="menu-item" :class="{active: $route.name == 'Authors'}">
+                    <router-link to="/authors"><span>Authors</span></router-link>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <router-view/>
 </template>
 
 <style lang="css">
+.main-menu {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 330px;
+}
+.menu-item {
+    width: 100px;
+    height: 100px;
+    border: 1px solid #000;
+}
+.menu-item a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+}
+.menu-item a:hover {
+    background-color: #eaeafd;
+    color: #63b6ff;
+}
+.menu-item a > span {
+    display: inline-block;
+    line-height: normal;
+    margin-top: 38%;
+}
 .active {
     background-color: #eaeafd;
 }
 a {
-  text-decoration: none;
-  color: #63b6ff;
+    text-decoration: none;
+    color: #63b6ff;
 }
 a:hover {
-  color: red;
-}
-.menu {
-  list-style: none;
-  display: block;
-  margin-top: 10px;
-  margin-bottom: 30px;
-}
-.menu li {
-  display: inline-block;
-  padding: 20px;
-  border: 1px solid #000;
-  margin-left: 5px;
-  cursor: pointer;
-}
-.menu li:first {
-  margin-left: 0;
+    color: red;
 }
 </style>
